9a43dcd47db00d07a7d7145d432e97fe4e39cba6
EJLEMEHT <Maksana009@gmail.com>
2023-03-20 22:41:21 +0500

    Добавление БД регистрации и входа

---
+++ database.py
@@ -0,0 +1,46 @@
+import sqlite3
+import hashlib
+
+
+# Функция для хэширования пароля
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
+def db_connection(email, password):
+    conn = sqlite3.connect('static/db/users.db')
+    c = conn.cursor()
+    # Хэширование пароля
+    hashed_password = hash_password(password)
+
+    # Проверка наличия имени пользователя в БД
+    c.execute('SELECT * FROM users WHERE email=?', (email,))
+    data = c.fetchone()
+    return hashed_password, data, c, conn
+
+def db_reg(username, email, password):
+    if len(username) > 15:
+        return 'TOO_MANY_SYMBOLS'
+    # Соединение с БД
+    hashed_password, data, c, conn = db_connection(email, password)
+
+    # Если имя пользователя ещё не существует в БД, то проверяем пароль на соответствие
+    if data:
+        conn.close()
+        return 'EMAIL_EXISTS'
+    else:
+        # Добавление новой записи в БД
+        c.execute('INSERT INTO users VALUES (?, ?, ?)', (username, email, hashed_password))
+        conn.commit()
+        conn.close()
+        return False
+
+
+def db_log(email, password):
+    # Соединение с БД
+    hashed_password, data, _, conn = db_connection(email, password)
+    conn.close()
+    # Если имя пользователя существует в БД, то проверяем пароль на соответствие
+    if data and hashed_password == data[2]:
+        return False
+    return True
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask_wtf.csrf import CSRFProtect
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField, SelectField, PasswordField, IntegerField
 from wtforms.validators import DataRequired
+from database import *
 
 
 app = Flask(__name__)
@@ -18,15 +19,27 @@ def unregistered():
 
 @app.route('/log-in', methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if not db_log(email, password):
+            return render_template('base.html')
+        return 'Неправильное имя или пароль'
     return render_template('log-in.html')
 
 
 @app.route('/reg-in', methods=['POST', 'GET'])
 def regin():
     if request.method == 'POST':
-        req = request.form.get('password-1')
-        print(req)
-        return 'dddddddddddddddddddd'
+        nickname = request.form.get('nickname')
+        email = request.form.get('email')
+        password = request.form.get('password-1')
+        reg = db_reg(nickname, email, password)
+        if not reg:
+            return render_template('base.html')
+        elif reg == 'TOO_MANY_SYMBOLS':
+            return 'МНогА'
+        return 'Занято'
     return render_template('reg-in.html')
 
 
